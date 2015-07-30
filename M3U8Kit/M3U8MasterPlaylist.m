5e8c0066a6d7ee8a9f7da1fc1569c74145758a4c
Alex Zchut <alex.zchut@kaltura.com>
2015-07-30 16:38:17 +0300

    update to last commit

--- M3U8Kit/M3U8MasterPlaylist.m
+++ M3U8Kit/M3U8MasterPlaylist.m
@@ -94,7 +94,7 @@
             M3U8ExtXStreamInf *lastStreamInf = self.xStreamList.lastXStreamInf;
             if (lastStreamInf) {
                 if ([lastStreamInf.URI isEqualToString:line]) {
-                    lastStreamInf
+                    [lastStreamInf appendToOriginalLines:line];
                 }
             }
         }
